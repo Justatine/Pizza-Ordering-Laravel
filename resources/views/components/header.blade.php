ba1e22be545ba78da150abc84b89b968493e76f6
unknown <tagaanjustinemark3@gmail.com>
2024-08-03 21:43:16 +0800

    fix: logout animation with 3s delay

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -15,14 +15,54 @@
 				<li class="nav-item {{ Request::is('cart') ? 'active' : '' }}"><a href="{{ url('/cart') }}" class="nav-link"><i class="fa fa-shopping-cart"></i> Cart</a></li>
 				<li class="nav-item {{ Request::is('profile') ? 'active' : '' }}"><a href="{{ url('/profile') }}" class="nav-link"><i class="fa fa-user"></i> Profile</a></li>
 				<li class="nav-item">
-					<a href="{{ url('/logout') }}" class="nav-link">
+					{{-- {{ url('/logout') }} --}}
+					<a href="#" class="nav-link" id="logout-link">
 					<i class="fa fa-sign-out"></i>  Sign out
 					</a>
 				</li>
 			@else
-				<li class="nav-item {{ Request::is('signin') ? 'active' : '' }}"><a href="{{ url('/signin') }}" class="nav-link"><i class="fa fa-sign-in"></i> Sign in</a></li>
+				<li class="nav-item">
+					<a href="{{ url('/signin') }}" class="nav-link {{ Request::is('signin') ? 'active' : '' }}" ><i class="fa fa-sign-in"></i> Sign in
+					</a>
+				</li>
 			@endif
 			</ul>
 		</div>
 	</div>
 </nav>
+<script>
+	document.addEventListener('DOMContentLoaded', function () {
+		document.getElementById('logout-link').addEventListener('click', function () {
+			Swal.fire({
+				// title: "CONFIRM",
+				text: "Are you sure to logout?",
+				icon: "warning",
+				showCancelButton: true,
+				confirmButtonColor: "#3085d6",
+				cancelButtonColor: "#d33",
+				confirmButtonText: "Yes"
+			}).then((result) => {
+				if (result.isConfirmed) {
+					const Toast = Swal.mixin({
+						toast: true,
+						position: "top-end",
+						showConfirmButton: false,
+						timer: 3000,
+						timerProgressBar: true,
+						didOpen: (toast) => {
+							toast.onmouseenter = Swal.stopTimer;
+							toast.onmouseleave = Swal.resumeTimer;
+						}
+					});
+					Toast.fire({
+						icon: "success",
+						title: "Logged out successfully"
+					});
+					setTimeout(() => {
+						window.location.href = "{{ url('/logout') }}"
+					}, 3000);
+				}
+			});
+		});
+	});
+</script>
